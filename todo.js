696b8b888dceccd58290a73e425aff6e7af1d805
Dustin Yu <dustin520@gmail.com>
2014-07-15 09:22:26 -0700

    added comments/changed ids

--- todo.js
+++ todo.js
@@ -1,29 +1,35 @@
 window.onload = function() {
 
+	// State Vars
 	var form = document.getElementById('form');
-	var task = document.getElementById('taskList');
-	var add = document.getElementById('addItem');
-	var items = document.getElementById('displayList');
+	var inputField = document.getElementById('listInput');
+	var submit = document.getElementById('submitButton');
+	var tasks = document.getElementById('displayList');
 
+	// event listener
 	form.onsubmit = function(event) {
 		event.preventDefault();
+
+		// creating new elements and nodes
 		var newItem = document.createElement('li');
-		var text = document.createTextNode(task.value);
+		var text = document.createTextNode(inputField.value);
 
 		var rm = document.createElement('button');
 		var check = document.createTextNode("X");
 
-
-		items.appendChild(newItem);
+		//appending elements and nodes
+		tasks.appendChild(newItem);
 		newItem.appendChild(text);
 
 		rm.appendChild(check);
 		newItem.appendChild(rm);
 
-
+		// remove items function 
 		rm.onclick = function() {
+		// removes li element and everything appended in it
 		newItem.remove();
 	}
+	// reset after each added item
 	this.reset();
 	}
 
